610e88d589efa849a635374716622c819b9ca218
AttractiveWatermeleon <59001136+AttractiveWatermeleon@users.noreply.github.com>
2020-01-05 14:27:27 +1100

    Commit pt.1 of Xander's mistake
    Don't know how to commit to multiple different locations easily so I'll just do them one by one. If something is broken tell me! Means I probably forgot a file.

--- dotaMod/dotaMod.cs
+++ dotaMod/dotaMod.cs
@@ -1,4 +1,19 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Graphics;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Terraria;
+using Terraria.GameContent.Dyes;
+using Terraria.GameContent.UI;
+using Terraria.Graphics.Effects;
+using Terraria.Graphics.Shaders;
+using Terraria.ID;
+using Terraria.Localization;
 using Terraria.ModLoader;
+using Terraria.UI;
+using static Terraria.ModLoader.ModContent;
 
 namespace dotaMod
 {
@@ -6,6 +21,15 @@ namespace dotaMod
 	{
 		public dotaMod()
 		{
+
 		}
+
+		public override void Load()
+        {
+			if (!Main.dedServ)
+            {
+				AddMusicBox(GetSoundSlot(SoundType.Music, "Sounds/Music/Docking"), ItemType("LunaSurfboard"), TileType("DockingMusicBox"));
+            }
+        }
 	}
 }
